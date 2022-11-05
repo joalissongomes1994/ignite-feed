20e056ca9aa584f2b4572cd4264e4b3026bc45b6
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-11-05 16:02:46 -0300

    React engines - Iterating in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,67 @@ import { Sidebar } from "./components/Sidebar";
 import styles from "./App.module.css";
 import "./global.css";
 
+// author : {avatarUrl: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/joalissongomes1994.png",
+      name: "Joalisson Gomes",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content: `Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+      no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀`,
+      },
+      { type: "link", content: "joalisson.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-11-05 15:00:39"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content: `Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+      no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀`,
+      },
+      { type: "link", content: "diego.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content: `Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+      no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀`,
+      },
+      { type: "link", content: "maykbrito.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-06-11 20:00:00"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -14,8 +75,14 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
