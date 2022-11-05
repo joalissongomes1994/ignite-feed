968fb6972fd1bd482cff466207027069f5e5ae22
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-11-05 05:19:52 -0300

    Application Structure - Component: Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,58 @@
+import { Comment } from "./Comment";
+import { Avatar } from "./Avatar";
+
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src="https://github.com/joalissongomes1994.png" />
+          <div className={styles.authorInfo}>
+            <strong>Joalisson Gomes</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="4 de Novembro às 08:13h" dateTime="2022-11-04 08:13:39">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          <a href="#"> jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe seu comentário" />
+
+        <footer>
+          <button type="submit">Comentar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  );
+}
