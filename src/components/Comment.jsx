0c176ab97d99fbbb467e6b67d00e5bfe89570f81
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-11-05 05:16:03 -0300

    Application Structure - Component: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,44 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/joalissongomes1994.png"
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Joalisson Gomes</strong>
+              <time
+                title="4 de Novembro às 08:13h"
+                dateTime="2022-11-04 08:13:39"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
