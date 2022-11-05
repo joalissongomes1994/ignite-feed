50c656aa5e1b755e8b61aad541ae4e0ea1621e0a
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2022-11-05 05:13:42 -0300

    Application Structure - Component: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
 import styles from "./App.module.css";
 import "./global.css";
 
 function App() {
   return (
     <div>
+      <Header />
+
       <div className={styles.wrapper}>
+        <Sidebar />
         <main></main>
       </div>
     </div>
